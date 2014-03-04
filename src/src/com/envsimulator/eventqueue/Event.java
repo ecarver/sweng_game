1a3f738cfa026c09ab7a3f53449a05cefc481d6b
carverer <carverer@mail.uc.edu>
2014-03-03 19:51:28 -0500

    Renamed package in file

--- src/src/com/envsimulator/eventqueue/Event.java
+++ src/src/com/envsimulator/eventqueue/Event.java
@@ -1,8 +1,7 @@
 // Module Author: Eric Carver
 // Mon Mar  3 18:16:56 EST 2014
 
-package com.envsimulator.priorityqueue;
-
+package com.envsimulator.eventqueue;
 
 public abstract class Event {
     public Event(int priority) {
@@ -11,4 +10,7 @@ public abstract class Event {
     public Event() { this(5); }
 
     public int priority;
+    public static final enum TypePriority {
+        RENDER(0), INTERACT(2), MOVE(4), AGE(6);
+    }
 }
